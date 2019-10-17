cb8c11b0ec3c28470e7eb20a715b970510c9285c
Dr Nic Williams <drnicwilliams@gmail.com>
2019-10-17 14:20:18 +1000

    kube was complaining about 'pod updates may not change fields' when it included 'resources.requests' field

--- hacks/redeploy.sh
+++ hacks/redeploy.sh
@@ -10,6 +10,6 @@ kubectl delete secret eirini-x-starkandwayne-pancake-setupcertificate
 kubectl apply -f config/deployment-ns-default.yaml
 kubectl wait pod --for condition=Ready -l app=eirini-pancake-extension -n default
 sleep 5
-kubectl apply -f config/fakes/eirini_app_empty_vcap_services.yaml
 kubectl apply -f config/fakes/eirini_app_no_vcap_services.yaml
+kubectl apply -f config/fakes/eirini_app_empty_vcap_services.yaml
 kubectl apply -f config/fakes/eirini_app_vcap_services.yaml
