80d70288a9e4478cb2cc6baaf14a56790af02c6c
Dr Nic Williams <drnicwilliams@gmail.com>
2019-10-17 14:01:41 +1000

    2nd fake app with $VCAP_SERVICES={}

--- hacks/redeploy.sh
+++ hacks/redeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-kubectl delete -f config/fakes/eirini_app.yaml
+kubectl delete --wait=false -f config/fakes/eirini_app_no_vcap_services.yaml
+kubectl delete --wait=false -f config/fakes/eirini_app_empty_vcap_services.yaml
 kubectl delete -f config/deployment-ns-default.yaml
 kubectl delete mutatingwebhookconfigurations eirini-x-starkandwayne-pancake-mutating-hook-default
 kubectl delete secret eirini-x-starkandwayne-pancake-setupcertificate
@@ -8,4 +9,5 @@ kubectl delete secret eirini-x-starkandwayne-pancake-setupcertificate
 kubectl apply -f config/deployment-ns-default.yaml
 kubectl wait pod --for condition=Ready -l app=eirini-pancake-extension -n default
 sleep 5
-kubectl apply -f config/fakes/eirini_app.yaml
+kubectl apply -f config/fakes/eirini_app_empty_vcap_services.yaml
+kubectl apply -f config/fakes/eirini_app_no_vcap_services.yaml
