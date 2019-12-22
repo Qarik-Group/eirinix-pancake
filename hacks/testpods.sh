af4a58966e09339d9abdaa3a06f8346c6c85dac8
Dr Nic Williams <drnicwilliams@gmail.com>
2019-12-22 13:00:58 +1000

    delete test pods first

--- hacks/testpods.sh
+++ hacks/testpods.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+kubectl delete --wait=false -f config/fakes/eirini-fake-app-with-vcap-services.yaml
+kubectl delete --wait=false -f config/fakes/eirini-fake-app-no-vcap-services.yaml
+kubectl delete --wait=true  -f config/fakes/eirini-fake-app-empty-vcap-services.yaml
 kubectl apply -f config/fakes/eirini-fake-app-no-vcap-services.yaml
 kubectl apply -f config/fakes/eirini-fake-app-empty-vcap-services.yaml
 kubectl apply -f config/fakes/eirini-fake-app-with-vcap-services.yaml
